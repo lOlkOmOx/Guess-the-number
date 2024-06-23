f93ad78bf82d9697e084854998a272555fafac39
Martin <martinsivak.15@gmail.com>
2024-06-23 15:34:29 +0200

    implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {Form, Button, Modal} from "react-bootstrap"
 
 function App() {
 
-  const setRandomNumber = (minValue, maxValue) => {
-    let result = (Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue)
-    return result
+  const min = 1
+  const [max, setMax] = useState('100')
+
+  const setRandomNumber = (min, max) => {
+    console.log("hledám mezi čísly " + min + " a " + max)
+    let result = (Math.floor(Math.random() * (max - min + 1)) + min)
+    setHiddenNumber(result)
+    console.log("setRandomNumber result " + result)
   }
   
-  const [hiddenNumber, setHiddenNumber] = useState(0);
-
-  useEffect(() => {
-    setHiddenNumber(setRandomNumber(0, 100));
-  }, []);
-
+  const [hiddenNumber, setHiddenNumber] = useState('');
 
   const [inputValue, setInputValue] = useState('')
   const [oldInputs, setOldInputs] = useState([])
@@ -53,9 +53,9 @@ function App() {
     return lastFiveInputs.map((input, index) => (
       <div key={index}>
         {input > hiddenNumber ? (
-          <Button variant="secondary" style={{fontSize:"50px", marginBottom:"5px"}}>{input} &gt; X</Button>
+          <Button variant="outline-secondary" style={{fontSize:"20px", marginBottom:"5px"}}>🠋 My number is less than {input} 🠋</Button>
         ) : (
-          <Button variant="secondary" style={{fontSize:"50px", marginBottom:"5px"}}>{input} &lt; X</Button>
+          <Button variant="outline-secondary" style={{fontSize:"20px", marginBottom:"5px"}}>🠉 My number is bigger than {input} 🠉</Button>
         )}
       </div>
     ));
@@ -65,15 +65,29 @@ function App() {
     window.location.reload()
   }
 
+  const [starting, setStarting] = useState(true)
+
+  const startGame = (event) => {
+    event.preventDefault();
+    setStarting(false)
+    setRandomNumber(parseInt(min), parseInt(max))
+  }
+
+
+  const handleMaxValueChange = (event) => {
+    setMax(event.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Guess the number</h1>
       <br/>
-      <Form>
-        <Form.Control  onChange={handleNumberInput} value={inputValue}/>
-        <Button onClick={handleSubmit} variant="primary" type="submit">Zkontrolovat</Button>
+      <h3>Guessing number between {min} and {max}</h3>
+      <Form onSubmit={handleSubmit} className="GuessInput">
+        <Form.Control autoFocus  onChange={handleNumberInput} value={inputValue} className="GuessInputForm"/>
+        <Button className="GuessInputButton" variant="success" onClick={handleSubmit} type="submit" disabled={isNaN(inputValue) || inputValue == 0}>🡺</Button>
       </Form>
-
+      <br/>
       {renderLastFiveInputs()}
       
       <Modal show={win} centered className="Modal">
@@ -88,6 +102,23 @@ function App() {
           <Button variant="primary" onClick={handleNewGame} >
             Play again!
           </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={starting} centered className="Modal">
+        <Modal.Header as="h1">
+          <Modal.Title>Game settings</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form onSubmit={startGame}>
+          <Form.Control autoFocus onChange={handleMaxValueChange} placeholder="100"/>
+          <Button variant="primary" type="submit" onClick={startGame} disabled={isNaN(max) || min >= max}>
+            Start with {max}!
+          </Button>
+        </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          
         </Modal.Footer>
       </Modal>
        
